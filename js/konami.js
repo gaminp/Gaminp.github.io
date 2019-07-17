8089b7ad0c33ecedf82493a79edeeff96b45c2e1
Vinicius Carli Bustamanti <vbustamanti@coden.local>
2019-07-17 13:37:18 -0300

    Adiciona Evento

--- js/konami.js
+++ js/konami.js
@@ -84,6 +84,7 @@ onKonamiCode(function () {
   audio = new Audio('js/A.mp3');
   img = document.getElementById('imgkonami');
   img2 = document.getElementById('imgkonami2');
+  img3 = document.getElementById('imgkonami3');
   if (typeof audio.loop == 'boolean')
 {
     audio.loop = true;
@@ -95,6 +96,7 @@ else
         this.play();
     }, false);
 }
+  img3.src='https://cdn.discordapp.com/attachments/227141030355402752/601087412063436811/unknown.png';
   img2.src='https://cdn.discordapp.com/attachments/227141030355402752/568122732101763114/unknown.png';
   img.src='js/konami.png';
   audio.play();
